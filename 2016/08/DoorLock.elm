a8bae5fb935f289c40763e143de026fc2db32146
Jason Dew <jason.dew@gmail.com>
2016-12-11 21:42:41 -0500

    Fixing day 8 display

--- 2016/08/DoorLock.elm
+++ 2016/08/DoorLock.elm
@@ -35,7 +35,7 @@ type Msg
 
 initialInput : String
 initialInput =
-    "rect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 3\nrect 1x1\nrotate row y=0 by 3\nrect 2x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 3\nrect 2x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 3\nrect 2x1\nrotate row y=0 by 5\nrect 4x1\nrotate row y=0 by 5\nrotate column x=0 by 1"
+    "rect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 5\nrect 1x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 3\nrect 1x1\nrotate row y=0 by 3\nrect 2x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 3\nrect 2x1\nrotate row y=0 by 2\nrect 1x1\nrotate row y=0 by 3\nrect 2x1\nrotate row y=0 by 5\nrect 4x1\nrotate row y=0 by 5\nrotate column x=0 by 1\nrect 4x1\nrotate row y=0 by 10\nrotate column x=5 by 2\nrotate column x=0 by 1\nrect 9x1\nrotate row y=2 by 5\nrotate row y=0 by 5\nrotate column x=0 by 1\nrect 4x1\nrotate row y=2 by 5\nrotate row y=0 by 5\nrotate column x=0 by 1\nrect 4x1\nrotate column x=40 by 1\nrotate column x=27 by 1\nrotate column x=22 by 1\nrotate column x=17 by 1\nrotate column x=12 by 1\nrotate column x=7 by 1\nrotate column x=2 by 1\nrotate row y=2 by 5\nrotate row y=1 by 3\nrotate row y=0 by 5\nrect 1x3\nrotate row y=2 by 10\nrotate row y=1 by 7\nrotate row y=0 by 2\nrotate column x=3 by 2\nrotate column x=2 by 1\nrotate column x=0 by 1\nrect 4x1\nrotate row y=2 by 5\nrotate row y=1 by 3\nrotate row y=0 by 3\nrect 1x3\nrotate column x=45 by 1\nrotate row y=2 by 7\nrotate row y=1 by 10\nrotate row y=0 by 2\nrotate column x=3 by 1\nrotate column x=2 by 2\nrotate column x=0 by 1\nrect 4x1\nrotate row y=2 by 13\nrotate row y=0 by 5\nrotate column x=3 by 1\nrotate column x=0 by 1\nrect 4x1\nrotate row y=3 by 10\nrotate row y=2 by 10\nrotate row y=0 by 5\nrotate column x=3 by 1\nrotate column x=2 by 1\nrotate column x=0 by 1\nrect 4x1\nrotate row y=3 by 8\nrotate row y=0 by 5\nrotate column x=3 by 1\nrotate column x=2 by 1\nrotate column x=0 by 1\nrect 4x1\nrotate row y=3 by 17\nrotate row y=2 by 20\nrotate row y=0 by 15\nrotate column x=13 by 1\nrotate column x=12 by 3\nrotate column x=10 by 1\nrotate column x=8 by 1\nrotate column x=7 by 2\nrotate column x=6 by 1\nrotate column x=5 by 1\nrotate column x=3 by 1\nrotate column x=2 by 2\nrotate column x=0 by 1\nrect 14x1\nrotate row y=1 by 47\nrotate column x=9 by 1\nrotate column x=4 by 1\nrotate row y=3 by 3\nrotate row y=2 by 10\nrotate row y=1 by 8\nrotate row y=0 by 5\nrotate column x=2 by 2\nrotate column x=0 by 2\nrect 3x2\nrotate row y=3 by 12\nrotate row y=2 by 10\nrotate row y=0 by 10\nrotate column x=8 by 1\nrotate column x=7 by 3\nrotate column x=5 by 1\nrotate column x=3 by 1\nrotate column x=2 by 1\nrotate column x=1 by 1\nrotate column x=0 by 1\nrect 9x1\nrotate row y=0 by 20\nrotate column x=46 by 1\nrotate row y=4 by 17\nrotate row y=3 by 10\nrotate row y=2 by 10\nrotate row y=1 by 5\nrotate column x=8 by 1\nrotate column x=7 by 1\nrotate column x=6 by 1\nrotate column x=5 by 1\nrotate column x=3 by 1\nrotate column x=2 by 2\nrotate column x=1 by 1\nrotate column x=0 by 1\nrect 9x1\nrotate column x=32 by 4\nrotate row y=4 by 33\nrotate row y=3 by 5\nrotate row y=2 by 15\nrotate row y=0 by 15\nrotate column x=13 by 1\nrotate column x=12 by 3\nrotate column x=10 by 1\nrotate column x=8 by 1\nrotate column x=7 by 2\nrotate column x=6 by 1\nrotate column x=5 by 1\nrotate column x=3 by 1\nrotate column x=2 by 1\nrotate column x=1 by 1\nrotate column x=0 by 1\nrect 14x1\nrotate column x=39 by 3\nrotate column x=35 by 4\nrotate column x=20 by 4\nrotate column x=19 by 3\nrotate column x=10 by 4\nrotate column x=9 by 3\nrotate column x=8 by 3\nrotate column x=5 by 4\nrotate column x=4 by 3\nrotate row y=5 by 5\nrotate row y=4 by 5\nrotate row y=3 by 33\nrotate row y=1 by 30\nrotate column x=48 by 1\nrotate column x=47 by 5\nrotate column x=46 by 5\nrotate column x=45 by 1\nrotate column x=43 by 1\nrotate column x=38 by 3\nrotate column x=37 by 3\nrotate column x=36 by 5\nrotate column x=35 by 1\nrotate column x=33 by 1\nrotate column x=32 by 5\nrotate column x=31 by 5\nrotate column x=30 by 1\nrotate column x=23 by 4\nrotate column x=22 by 3\nrotate column x=21 by 3\nrotate column x=20 by 1\nrotate column x=12 by 2\nrotate column x=11 by 2\nrotate column x=3 by 5\nrotate column x=2 by 5\nrotate column x=1 by 3\nrotate column x=0 by 4"
 
 
 init : ( Model, Cmd Msg )
@@ -249,7 +249,7 @@ gridView : Grid -> Html Msg
 gridView grid =
     grid
         |> List.map rowView
-        |> table []
+        |> table [ style [ ( "border-collapse", "collapse" ) ] ]
 
 
 rowView : List Cell -> Html Msg
@@ -261,12 +261,16 @@ rowView row =
 
 cellView : Cell -> Html Msg
 cellView cell =
-    case cell of
-        On ->
-            td [ style [ ( "background", "red" ) ] ] [ text "" ]
+    let
+        color =
+            case cell of
+                On ->
+                    "red"
 
-        Off ->
-            td [ style [ ( "background", "black" ) ] ] [ text "" ]
+                Off ->
+                    "black"
+    in
+        td [ style [ ( "background", color ), ( "width", "1em" ), ( "height", "1em" ) ] ] [ text "" ]
 
 
 instructionsView : List Instruction -> Html Msg
