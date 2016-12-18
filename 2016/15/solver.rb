1b06948b58cfd08df29db3ba7df21d90aeb92cef
Jason Dew <jason.dew@gmail.com>
2016-12-18 10:42:48 -0500

    Day 15, part 1

---
+++ 2016/15/solver.rb
@@ -0,0 +1,7 @@
+def d1(n); (n+1) % 17; end
+def d2(n); n % 7; end
+def d3(n); (n + 2) % 19; end
+def d4(n); n % 5; end
+def d5(n); n % 3; end
+def d6(n); (n + 5) % 13; end
+(0..350_000).detect {|t| d1(t+1) + d2(t+2) + d3(t+3) + d4(t+4) + d5(t+5) + d6(t+6) == 0 }
