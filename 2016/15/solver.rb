dbfe8163e33f03b1b1aa404bbc928996c6f95b77
Jason Dew <jason.dew@gmail.com>
2016-12-18 15:39:19 -0500

    Adding day 15, part 2

--- 2016/15/solver.rb
+++ 2016/15/solver.rb
@@ -4,4 +4,6 @@ def d3(n); (n + 2) % 19; end
 def d4(n); n % 5; end
 def d5(n); n % 3; end
 def d6(n); (n + 5) % 13; end
-(0..350_000).detect {|t| d1(t+1) + d2(t+2) + d3(t+3) + d4(t+4) + d5(t+5) + d6(t+6) == 0 }
+def d7(n); n % 11; end
+
+p (0..1_000_000_000).detect {|t| d1(t+1) + d2(t+2) + d3(t+3) + d4(t+4) + d5(t+5) + d6(t+6) + d7(t+7) == 0 }
