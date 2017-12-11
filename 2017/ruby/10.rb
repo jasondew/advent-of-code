107aca3cff05ab4a0b8ba2a161188fd8dce26610
Jason Dew <jason.dew@gmail.com>
2017-12-10 20:57:24 -0500

    Day 10, part 1

--- 2017/ruby/10.rb
+++ 2017/ruby/10.rb
@@ -1,44 +1,41 @@
 require "ap"
 
 def part_one(input, size)
-  lengths = input.strip.split(" ").map(&:to_i)
+  lengths = input.strip.split(",").map(&:to_i)
+  p lengths
   initial_list = (0..size-1).to_a
   context = { list: initial_list, position: 0, skip_size: 0 }
 
   final_context = lengths.each_with_object(context) do |length, context|
-    puts "length: #{length} context: #{context}"
-    list_length = context[:list].length
+    puts "length: #{length}"
+
+    list = context[:list]
+    list_length = list.length
     from = context[:position]
-    to = context[:position] + length
+    to = context[:position] + length - 1
 
-    puts "from: #{from} to: #{to} list_length: #{list_length}"
-
-    if to == from + 1
-      # no-op
-    elsif to >= list_length
+    if to >= list_length
       to_end = list_length - 1
       to_wrapped = to % list_length
+      new_sequence = (list[from..to_end] + list[0..to_wrapped]).reverse
 
-      puts "(#{from}, #{to_end}) -> (0, #{to_wrapped})"
-      puts "(0, #{to_wrapped}) -> (0, #{to_wrapped})"
-
-      context[:list][0, to_wrapped], context[:list][from, to_end] =
-        context[:list][from, to_end].reverse, context[:list][0, to_wrapped].reverse
+      list[from..to_end] = new_sequence[0..(to_end-from)]
+      list[0..to_wrapped] = new_sequence[(to_end-from+1)..(new_sequence.length-1)]
     else
-      context[:list][from, to] = context[:list][from, to].reverse
+      list[from..to] = list[from..to].reverse
     end
 
     context[:position] = (context[:position] + length + context[:skip_size]) % list_length
     context[:skip_size] += 1
   end
 
-  puts final_context[:list].inspect
+  puts "final list: #{final_context[:list]}"
 
   final_context[:list].first(2).inject(:*)
 end
 
-input = "3 4 1 5"
+input = "3,4,1,5"
 puts "multiplied first two: #{part_one(input, 5)}"
 
-#input = File.read("../inputs/10").chomp
-#puts "multiplied first two: #{part_one(input, 256)}"
+input = File.read("../inputs/10").chomp
+puts "multiplied first two: #{part_one(input, 256)}"
