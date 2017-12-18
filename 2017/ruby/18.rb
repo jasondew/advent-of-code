bc73b1363f6678aa76d775d2012e91a9869d2b32
Jason Dew <jason.dew@gmail.com>
2017-12-18 08:46:34 -0500

    Day 18, part 1

--- 2017/ruby/18.rb
+++ 2017/ruby/18.rb
@@ -1,54 +1,58 @@
 require "ap"
+require "pry"
 
 Play = Struct.new(:register) do
   def execute(registers)
-    registers[:lp] = registers[register]
-    registers[:ip] += 1
+    registers[:lp] = registers.fetch(register)
+    registers[:pc] += 1
   end
 end
 
 Set = Struct.new(:register, :value) do
   def execute(registers)
-    registers[register] = value
-    registers[:ip] += 1
+    registers[register] = evaluate(registers, value)
+    registers[:pc] += 1
   end
 end
 
 Add = Struct.new(:register, :value) do
   def execute(registers)
-    registers[register] += value
-    registers[:ip] += 1
+    registers[register] += evaluate(registers, value)
+    registers[:pc] += 1
   end
 end
 
 Multiply = Struct.new(:register, :value) do
   def execute(registers)
-    registers[register] *= value
-    registers[:ip] += 1
+    registers[register] *= evaluate(registers, value)
+    registers[:pc] += 1
   end
 end
 
 Modulo = Struct.new(:register, :value) do
   def execute(registers)
-    registers[register] = registers[register] % value
-    registers[:ip] += 1
+    registers[register] %= evaluate(registers, value)
+    registers[:pc] += 1
   end
 end
 
 Recover = Struct.new(:register) do
   def execute(registers)
-    last_played = registers[:lp]
-    puts last_played unless last_played.zero?
-    registers[:ip] += 1
+    if registers[register].nonzero? && registers[:lp].nonzero?
+      puts "Recovered #{registers[:lp]}"
+      registers[:pc] = -1
+    else
+      registers[:pc] += 1
+    end
   end
 end
 
-Jump = Struct.new(:register, :offset) do
+Jump = Struct.new(:register_or_value, :value) do
   def execute(registers)
-    if registers[register] > 0
-      registers[:ip] += offset
+    if evaluate(registers, register_or_value) > 0
+      registers[:pc] += evaluate(registers, value)
     else
-      registers[:ip] += 1
+      registers[:pc] += 1
     end
   end
 end
@@ -61,35 +65,63 @@ def parse_instruction(input)
   parts = input.split(/ /)
   case parts[0]
   when "snd" then Play.new(parts[1].to_sym)
-  when "set" then Set.new(parts[1].to_sym, parts[2].to_i)
-  when "add" then Add.new(parts[1].to_sym, parts[2].to_i)
-  when "mul" then Multiply.new(parts[1].to_sym, parts[2].to_i)
-  when "mod" then Modulo.new(parts[1].to_sym, parts[2].to_i)
+  when "set" then Set.new(parts[1].to_sym, parts[2])
+  when "add" then Add.new(parts[1].to_sym, parts[2])
+  when "mul" then Multiply.new(parts[1].to_sym, parts[2])
+  when "mod" then Modulo.new(parts[1].to_sym, parts[2])
   when "rcv" then Recover.new(parts[1].to_sym)
-  when "jgz" then Jump.new(parts[1].to_sym, parts[2].to_i)
+  when "jgz" then Jump.new(parts[1], parts[2])
+  else raise "Unrecognized instruction: #{parts.inspect}"
   end
 end
 
 def initial_registers
-  %i{a b c d e f g h i j k l m n o p ip lp}.map {|register| [register, 0] }.to_h
+  %i{a b c d e f g h i j k l m n o p pc lp}.map {|register| [register, 0] }.to_h
+end
+
+def evaluate(registers, value_or_register)
+  if value_or_register =~ /-?\d+/
+    Integer(value_or_register)
+  else
+    if value = registers[value_or_register.to_sym]
+      value
+    else
+      raise "Invalid value_or_register: #{value_or_register.inspect}"
+    end
+  end
 end
 
 def execute(instructions)
   registers = initial_registers
 
   loop do
-    instruction = instructions[registers[:ip]]
-    p instruction
+    instruction = instructions[registers[:pc]]
+
+    if $debug
+      puts "#{registers[:pc]}: #{instruction.inspect}"
+      puts "-"*80
+    end
+
     instruction.execute(registers)
-    p registers[:ip]
-    break if registers[:ip] > instructions.size
+
+    if $debug
+      p registers
+      puts
+    end
+
+    break if halt_condition?(registers, instructions)
   end
+
+  registers
+end
+
+def halt_condition?(registers, instructions)
+  registers[:pc] >= instructions.size || registers[:pc] < 0
 end
 
 def part_one(input)
   instructions = parse(input)
-  ap instructions
-  execute(instructions)
+  registers = execute(instructions)
 end
 
 example_input = <<-EOT
@@ -104,5 +136,8 @@ jgz a -1
 set a 1
 jgz a -2
 EOT
+puzzle_input = File.read("../inputs/18").chomp
 
-part_one(example_input)
+$debug = false
+#part_one(example_input)
+part_one(puzzle_input)
