aadcd5e04aa445b962b7920a63e886c780c942ce
Jason Dew <jason.dew@gmail.com>
2017-12-18 18:36:09 -0500

    Day 18, part 2

--- 2017/ruby/18.rb
+++ 2017/ruby/18.rb
@@ -1,13 +1,6 @@
 require "ap"
 require "pry"
 
-Play = Struct.new(:register) do
-  def execute(registers)
-    registers[:lp] = registers.fetch(register)
-    registers[:pc] += 1
-  end
-end
-
 Set = Struct.new(:register, :value) do
   def execute(registers)
     registers[register] = evaluate(registers, value)
@@ -36,17 +29,6 @@ Modulo = Struct.new(:register, :value) do
   end
 end
 
-Recover = Struct.new(:register) do
-  def execute(registers)
-    if registers[register].nonzero? && registers[:lp].nonzero?
-      puts "Recovered #{registers[:lp]}"
-      registers[:pc] = -1
-    else
-      registers[:pc] += 1
-    end
-  end
-end
-
 Jump = Struct.new(:register_or_value, :value) do
   def execute(registers)
     if evaluate(registers, register_or_value) > 0
@@ -57,26 +39,69 @@ Jump = Struct.new(:register_or_value, :value) do
   end
 end
 
-def parse(input)
-  input.split(/\n/).map(&method(:parse_instruction))
+Play = Struct.new(:register) do
+  def execute(registers)
+    registers[:outgoing] << registers.fetch(register)
+    registers[:pc] += 1
+  end
 end
 
-def parse_instruction(input)
-  parts = input.split(/ /)
+Recover = Struct.new(:register) do
+  def execute(registers)
+    if registers[register].nonzero? && registers[:outgoing].size.nonzero?
+      puts "Recovered #{registers[:outgoing].last}"
+      registers[:pc] = -1
+    else
+      registers[:pc] += 1
+    end
+  end
+end
+
+Send = Struct.new(:register) do
+  def execute(registers)
+    registers[:outgoing] << evaluate(registers, register)
+    registers[:outgoing_count] += 1
+    registers[:pc] += 1
+  end
+end
+
+Receive = Struct.new(:register) do
+  def execute(registers)
+    if registers[:incoming].size.nonzero?
+      registers[:waiting] = false
+      registers[register] = registers[:incoming].shift
+      registers[:pc] += 1
+    else
+      registers[:waiting] = true
+    end
+  end
+end
+
+def parse(input, send_klass, receive_klass)
+  input.split(/\n/).map do |line|
+    parse_instruction(line, send_klass, receive_klass)
+  end
+end
+
+def parse_instruction(line, send_klass, receive_klass)
+  parts = line.split(/ /)
   case parts[0]
-  when "snd" then Play.new(parts[1].to_sym)
   when "set" then Set.new(parts[1].to_sym, parts[2])
   when "add" then Add.new(parts[1].to_sym, parts[2])
   when "mul" then Multiply.new(parts[1].to_sym, parts[2])
   when "mod" then Modulo.new(parts[1].to_sym, parts[2])
-  when "rcv" then Recover.new(parts[1].to_sym)
   when "jgz" then Jump.new(parts[1], parts[2])
+  when "snd" then send_klass.new(parts[1].to_sym)
+  when "rcv" then receive_klass.new(parts[1].to_sym)
   else raise "Unrecognized instruction: #{parts.inspect}"
   end
 end
 
 def initial_registers
-  %i{a b c d e f g h i j k l m n o p pc lp}.map {|register| [register, 0] }.to_h
+  %i{a b c d e f g h i j k l m n o p pc}.
+    map {|register| [register, 0] }.
+    to_h.
+    merge(incoming: [], outgoing: [], outgoing_count: 0, waiting: false)
 end
 
 def evaluate(registers, value_or_register)
@@ -95,24 +120,27 @@ def execute(instructions)
   registers = initial_registers
 
   loop do
-    instruction = instructions[registers[:pc]]
-
-    if $debug
-      puts "#{registers[:pc]}: #{instruction.inspect}"
-      puts "-"*80
-    end
-
-    instruction.execute(registers)
-
-    if $debug
-      p registers
-      puts
-    end
-
+    execute_instruction(instructions, registers)
     break if halt_condition?(registers, instructions)
   end
 
   registers
+end
+
+def execute_instruction(instructions, registers)
+  instruction = instructions[registers[:pc]]
+
+  if $debug
+    puts "#{registers[:pc]}: #{instruction.inspect}"
+    puts "-"*80
+  end
+
+  instruction.execute(registers)
+
+  if $debug
+    p registers
+    puts
+  end
 end
 
 def halt_condition?(registers, instructions)
@@ -120,8 +148,35 @@ def halt_condition?(registers, instructions)
 end
 
 def part_one(input)
-  instructions = parse(input)
+  instructions = parse(input, Play, Recover)
   registers = execute(instructions)
+end
+
+def part_two(input)
+  instructions = parse(input, Send, Receive)
+
+  program_zero = initial_registers.merge(p: 0)
+  program_one = initial_registers.merge(p: 1)
+
+  1_000_000.times do
+    execute_instruction(instructions, program_zero)
+    execute_instruction(instructions, program_one)
+
+    if program_zero[:outgoing].size.nonzero?
+      program_one[:incoming] += program_zero[:outgoing]
+      program_zero[:outgoing] = []
+    end
+
+    if program_one[:outgoing].size.nonzero?
+      program_zero[:incoming] += program_one[:outgoing]
+      program_one[:outgoing] = []
+    end
+
+    break if (halt_condition?(program_zero, instructions) && halt_condition?(program_one, instructions)) ||
+             (program_zero[:waiting] && program_one[:waiting])
+  end
+
+  puts "Program 1 sends: #{program_one[:outgoing_count]}"
 end
 
 example_input = <<-EOT
@@ -139,5 +194,6 @@ EOT
 puzzle_input = File.read("../inputs/18").chomp
 
 $debug = false
-#part_one(example_input)
+part_one(example_input)
 part_one(puzzle_input)
+part_two(puzzle_input)
