aac0ddb671f2a1c54f684677c09b3b05963229cc
Jason Dew <jason.dew@gmail.com>
2019-12-02 10:37:39 -0500

    Adding day 2 in Elixir

--- 2019/elixir/lib/advent_of_code2019.ex
+++ 2019/elixir/lib/advent_of_code2019.ex
@@ -10,9 +10,9 @@ defmodule AdventOfCode2019 do
   end
 
   def day_two_answers do
-    input = day_input("02") |> to_integers()
-    IO.puts("Day 1, part 1 answer: #{DayOne.part_one(input)}")
-    IO.puts("Day 1, part 2 answer: #{DayOne.part_two(input)}")
+    input = day_input("02") |> to_integers(",")
+    IO.puts("Day 2, part 1 answer: #{DayTwo.part_one(input)}")
+    IO.puts("Day 2, part 2 answer: #{DayTwo.part_two(input)}")
   end
 
   ## PRIVATE FUNCTIONS ##
@@ -22,9 +22,9 @@ defmodule AdventOfCode2019 do
     if trim, do: String.trim(input), else: input
   end
 
-  defp to_integers(input) do
+  defp to_integers(input, delimiter \\ "\n") do
     input
-    |> String.split()
+    |> String.split(delimiter)
     |> Enum.map(&String.to_integer/1)
   end
 end
--- 2019/elixir/lib/day_two.ex
+++ 2019/elixir/lib/day_two.ex
@@ -1,25 +1,66 @@
 defmodule DayTwo do
   @doc """
     Part 1
-
-    iex> DayTwo.part_one([12, 14, 1_969, 100_756])
-    12
   """
 
-  def part_one(state) do
-    state
-    |> List.first()
+  def part_one(input) do
+    run(input, 12, 2)
   end
 
   @doc """
     Part 2
-
-    iex> DayTwo.part_two([12, 14, 1_969, 100_756])
-    12
   """
 
-  def part_two(state) do
-    state
+  def part_two(input) do
+    all_nouns_and_verbs =
+      for noun <- 0..99, verb <- 0..99 do
+        {noun, verb}
+      end
+
+    {noun, verb} =
+      Enum.find(all_nouns_and_verbs, fn {noun, verb} ->
+        run(input, noun, verb) == 19_690_720
+      end)
+
+    noun * 100 + verb
+  end
+
+  @doc """
+    iex> DayTwo.run([1,9,10,3,2,3,11,0,99,30,40,50], 9, 10)
+    3500
+  """
+
+  def run(input, noun, verb) do
+    input
+    |> List.replace_at(1, noun)
+    |> List.replace_at(2, verb)
+    |> step(0)
     |> List.first()
+  end
+
+  @doc """
+    iex> DayTwo.step([1,9,10,3,2,3,11,0,99,30,40,50], 0)
+    [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+  """
+
+  def step(input, position) do
+    [operation, arg1_position, arg2_position, result_position] = Enum.slice(input, position, 4)
+    arg1 = Enum.at(input, arg1_position)
+    arg2 = Enum.at(input, arg2_position)
+
+    case operation do
+      1 ->
+        input
+        |> List.replace_at(result_position, arg1 + arg2)
+        |> step(position + 4)
+
+      2 ->
+        input
+        |> List.replace_at(result_position, arg1 * arg2)
+        |> step(position + 4)
+
+      99 ->
+        input
+    end
   end
 end
