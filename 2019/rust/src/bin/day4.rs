a959cd55d70f3f528c65d50df95245ac540dd3ae
Jason Dew <jason.dew@gmail.com>
2019-12-05 22:50:18 -0500

    Day 4, part 2 in Rust

--- 2019/rust/src/bin/day4.rs
+++ 2019/rust/src/bin/day4.rs
@@ -1,15 +1,15 @@
 use std::fs;
 use std::io;
 
-fn to_digits(n: u32) -> Vec<u8> {
-    let mut digits: Vec<u8> = Vec::new();
+fn to_digits(n: u32) -> Vec<i8> {
+    let mut digits: Vec<i8> = Vec::new();
     let mut n = n;
 
     while n > 9 {
-        digits.push((n % 10) as u8);
+        digits.push((n % 10) as i8);
         n = n / 10;
     }
-    digits.push(n as u8);
+    digits.push(n as i8);
     digits.reverse();
 
     digits
@@ -24,30 +24,50 @@ fn main() -> io::Result<()> {
         .collect::<Vec<u32>>()
         .get(0..2)
     {
-        let mut count: u16 = 0;
+        let mut part1_count: u16 = 0;
+        let mut part2_count: u16 = 0;
+
         for number in *from..*to {
             let digits = to_digits(number);
-            let mut previous_digit: u8 = 0;
+            let mut previous_digit: i8 = -1;
             let mut non_decreasing = true;
             let mut two_in_a_row = false;
+            let mut strictly_two_in_a_row = false;
+            let mut duplicity = 1;
 
             digits.iter().for_each(|digit| {
-                if digit < &previous_digit {
+                if &previous_digit > digit {
                     non_decreasing = false
                 };
-                if digit == &previous_digit {
-                    two_in_a_row = true
+
+                if &previous_digit == digit {
+                    two_in_a_row = true;
+                    duplicity += 1;
+                } else {
+                    if duplicity == 2 {
+                        strictly_two_in_a_row = true
+                    };
+                    duplicity = 1;
                 };
 
-                previous_digit = *digit
+                previous_digit = *digit;
             });
 
+            if duplicity == 2 {
+                strictly_two_in_a_row = true
+            };
+
             if non_decreasing && two_in_a_row {
-                count = count + 1
-            }
+                part1_count += 1
+            };
+
+            if non_decreasing && strictly_two_in_a_row {
+                part2_count += 1
+            };
         }
 
-        println!("part 1: {}", count);
+        println!("part 1: {}", part1_count);
+        println!("part 2: {}", part2_count);
     }
 
     Ok(())
