5afc912e4cf779793a925c1887bd3e126e9d10ea
Jason Dew <jason.dew@gmail.com>
2020-12-10 18:48:19 -0500

    Adding 2020 Day 10 in Rust (and combining benchmarking functions)

---
+++ 2020/rust/benches/days.rs
@@ -0,0 +1,51 @@
+use aoc2020::*;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn day3(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Day 3");
+    let input = std::fs::read_to_string("../inputs/03").unwrap();
+
+    group.bench_function("part 1", |b| b.iter(|| day3::part1(black_box(&input))));
+    group.bench_function("part 2", |b| b.iter(|| day3::part2(black_box(&input))));
+    group.finish();
+}
+
+pub fn day4(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Day 4");
+    let input = std::fs::read_to_string("../inputs/04").unwrap();
+
+    group.bench_function("part 1", |b| b.iter(|| day4::part1(black_box(&input))));
+    group.bench_function("part 2", |b| b.iter(|| day4::part2(black_box(&input))));
+    group.finish();
+}
+
+pub fn day5(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Day 5");
+    let input = std::fs::read_to_string("../inputs/05").unwrap();
+
+    group.bench_function("part 1", |b| b.iter(|| day5::part1(black_box(&input))));
+    group.bench_function("part 2", |b| b.iter(|| day5::part2(black_box(&input))));
+    group.finish();
+}
+
+pub fn day6(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Day 6");
+    let input = std::fs::read_to_string("../inputs/06").unwrap();
+
+    group.bench_function("part 1", |b| b.iter(|| day6::part1(black_box(&input))));
+    group.bench_function("part 2", |b| b.iter(|| day6::part2(black_box(&input))));
+    group.finish();
+}
+
+pub fn day10(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Day 10");
+    let input = std::fs::read_to_string("../inputs/10").unwrap();
+
+    group.bench_function("part 1", |b| b.iter(|| day10::part1(black_box(&input))));
+    group.bench_function("part 2", |b| b.iter(|| day10::part2(black_box(&input))));
+    group.finish();
+}
+
+criterion_group!(benches, day3, day4, day5, day6, day10);
+criterion_main!(benches);
--- 2020/rust/src/day10.rs
+++ 2020/rust/src/day10.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 #[must_use]
 pub fn part1(input: &str) -> usize {
     let mut voltages: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
-    voltages.sort();
+    voltages.sort_unstable();
     let deltas: Vec<usize> = voltages
         .iter()
         .skip(1)
@@ -9,15 +11,58 @@ pub fn part1(input: &str) -> usize {
         .map(|(next, prev)| next - prev)
         .collect();
 
-    let delta_threes = deltas.iter().filter(|&delta| delta == &3usize).count();
-    let delta_ones = deltas.iter().filter(|&delta| delta == &1usize).count();
+    let delta_threes: usize = deltas.iter().filter(|&delta| delta == &3).count();
+    let delta_ones: usize = deltas.iter().filter(|&delta| delta == &1).count();
 
     (delta_threes + 1) * (delta_ones + 1)
 }
 
 #[must_use]
 pub fn part2(input: &str) -> usize {
-    input.len()
+    let mut voltages: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
+    voltages.sort_unstable();
+
+    let mut cache: HashMap<usize, usize> = HashMap::new();
+
+    chain_count(&voltages, 0, 1, &mut cache)
+}
+
+fn chain_count(
+    voltages: &[usize],
+    current: usize,
+    count: usize,
+    cache: &mut HashMap<usize, usize>,
+) -> usize {
+    if let Some(count) = cache.get(&current) {
+        *count
+    } else {
+        let (options, rest): (Vec<usize>, Vec<usize>) =
+            voltages.iter().partition(|&v| v <= &(current + 3));
+        let count = match options.as_slice() {
+            [] => count,
+            [x] => chain_count(&rest, *x, count, cache),
+            [x, y] => {
+                let a = chain_count(&rest, *y, count, cache);
+                let mut rr: Vec<usize> = rest.to_vec();
+                rr.insert(0, *y);
+                let b = chain_count(&rr, *x, count, cache);
+                a + b
+            }
+            [x, y, z] => {
+                let a = chain_count(&rest, *z, count, cache);
+                let mut rr: Vec<usize> = rest.to_vec();
+                rr.insert(0, *z);
+                let b = chain_count(&rr, *y, count, cache);
+                rr.insert(0, *y);
+                let c = chain_count(&rr, *x, count, cache);
+
+                a + b + c
+            }
+            _ => panic!(""),
+        };
+        cache.insert(current, count);
+        count
+    }
 }
 
 #[cfg(test)]
@@ -27,11 +72,12 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(part1("16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4\n"), 35);
-        assert_eq!(part1("28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3\n"), 220)
+        assert_eq!(part1("28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3\n"), 220);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2("quux"), 4)
+        assert_eq!(part2("16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4\n"), 8);
+        assert_eq!(part2("28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3\n"), 19208);
     }
 }
