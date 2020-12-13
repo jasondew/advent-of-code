784c3dc4fd57d82bb44c37462ec1aef3b79e0b5a
Jason Dew <jason.dew@gmail.com>
2020-12-13 10:16:44 -0500

    Adding 2020 Day 13 in Rust

--- 2020/rust/benches/days.rs
+++ 2020/rust/benches/days.rs
@@ -65,5 +65,14 @@ pub fn day12(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, day3, day4, day5, day6, day10, day11, day12);
+pub fn day13(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Day 13");
+    let input = std::fs::read_to_string("../inputs/13").unwrap();
+
+    group.bench_function("part 1", |b| b.iter(|| day13::part1(black_box(&input))));
+    group.bench_function("part 2", |b| b.iter(|| day13::part2(black_box(&input))));
+    group.finish();
+}
+
+criterion_group!(benches, day3, day4, day5, day6, day10, day11, day12, day13);
 criterion_main!(benches);
