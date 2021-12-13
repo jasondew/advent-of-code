baacab9d83f7c638044ebacb9dd4a74bef1179fa
Jason Dew <jason.dew@gmail.com>
2021-12-12 20:32:09 -0500

    Speeding up 2021 day 12

--- 2021/rust/src/day12.rs
+++ 2021/rust/src/day12.rs
@@ -2,18 +2,20 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 type Cave = String;
+type Path<'a> = Vec<&'a Cave>;
 
 #[must_use]
 pub fn part1(input: &str) -> usize {
     let connections = parse(input);
-    let small_caves_visitable_once = |next_cave: &Cave, path: &Vec<Cave>| -> bool {
-        large_cave(next_cave) || !path.iter().any(|cave| cave == next_cave)
+    let start_cave = "start".into();
+    let small_caves_visitable_once = |next_cave: &Cave, path: &Path| -> bool {
+        large_cave(next_cave) || !path.iter().any(|cave| *cave == next_cave)
     };
 
-    let all_paths: Vec<Vec<Cave>> = traverse(
+    let all_paths: Vec<Path> = traverse(
         &connections,
         &small_caves_visitable_once,
-        "start".into(),
+        &start_cave,
         vec![],
     )
     .into_iter()
@@ -23,27 +25,51 @@ pub fn part1(input: &str) -> usize {
     all_paths.len()
 }
 
-fn traverse(
-    connections: &HashMap<Cave, Vec<Cave>>,
-    visitable: &dyn Fn(&Cave, &Vec<Cave>) -> bool,
-    current_cave: Cave,
-    mut path: Vec<Cave>,
-) -> Vec<Vec<Cave>> {
-    path.push(current_cave.clone());
+#[must_use]
+pub fn part2(input: &str) -> usize {
+    let connections = parse(input);
+    let start_cave = "start".into();
+    let small_caves_visitable_twice = |next_cave: &Cave, path: &Path| -> bool {
+        next_cave != "start"
+            && (large_cave(next_cave)
+                || cave_not_seen(next_cave, &path)
+                || !small_cave_visited_twice(&path))
+    };
+
+    let all_paths: Vec<Path> = traverse(
+        &connections,
+        &small_caves_visitable_twice,
+        &start_cave,
+        vec![],
+    )
+    .into_iter()
+    .filter(|path| path[path.len() - 1] == "end")
+    .collect();
+
+    all_paths.len()
+}
+
+fn traverse<'a>(
+    connections: &'a HashMap<Cave, Vec<Cave>>,
+    visitable: &dyn Fn(&Cave, &Path) -> bool,
+    current_cave: &'a Cave,
+    mut path: Path<'a>,
+) -> Vec<Path<'a>> {
+    path.push(current_cave);
 
     if current_cave == "end" {
         return vec![path.clone()];
     }
 
     connections
-        .get(&current_cave)
+        .get(current_cave)
         .unwrap()
         .iter()
         .flat_map(|next_cave| {
             if visitable(next_cave, &path) {
-                traverse(connections, visitable, next_cave.clone(), path.clone())
+                traverse(connections, visitable, next_cave, path.clone())
             } else {
-                vec![path.clone()]
+                vec![]
             }
         })
         .collect()
@@ -53,21 +79,22 @@ fn large_cave(cave: &Cave) -> bool {
     cave.chars().any(|c| c.is_uppercase())
 }
 
-fn cave_not_seen(cave: &Cave, path: &Vec<Cave>) -> bool {
-    !path.iter().any(|c| c == cave)
+fn cave_not_seen(cave: &Cave, path: &Path) -> bool {
+    !path.iter().any(|c| *c == cave)
 }
 
-fn small_cave_visited_twice(path: &Vec<Cave>) -> bool {
-    let mut sorted_path: Vec<Cave> = path
-        .iter()
-        .filter(|cave| !large_cave(cave))
-        .cloned()
-        .collect();
-    sorted_path.sort_unstable();
-    sorted_path
-        .iter()
-        .zip(sorted_path.iter().skip(1))
-        .any(|(a, b)| a == b)
+fn small_cave_visited_twice(path: &Path) -> bool {
+    let mut visited: HashSet<&Cave> = HashSet::new();
+    for cave in path {
+        if !large_cave(cave) {
+            if let Some(_) = visited.get(cave) {
+                return true;
+            } else {
+                visited.insert(cave);
+            }
+        }
+    }
+    false
 }
 
 fn parse(input: &str) -> HashMap<Cave, Vec<Cave>> {
@@ -96,33 +123,6 @@ fn parse(input: &str) -> HashMap<Cave, Vec<Cave>> {
     }
 
     connections
-}
-
-#[must_use]
-pub fn part2(input: &str) -> usize {
-    let connections = parse(input);
-    let small_caves_visitable_twice = |next_cave: &Cave, path: &Vec<Cave>| -> bool {
-        next_cave != "start"
-            && (large_cave(next_cave)
-                || cave_not_seen(next_cave, &path)
-                || !small_cave_visited_twice(&path))
-    };
-
-    let all_paths: Vec<Vec<Cave>> = traverse(
-        &connections,
-        &small_caves_visitable_twice,
-        "start".into(),
-        vec![],
-    )
-    .into_iter()
-    .filter(|path| path[path.len() - 1] == "end")
-    .collect();
-
-    //    for path in &all_paths {
-    //        println!("{}", path.join(","));
-    //    }
-
-    all_paths.len()
 }
 
 #[cfg(test)]
