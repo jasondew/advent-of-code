037cff5d7b88dd513aad3723ae8593e4fa12aed6
Jason Dew <jason.dew@gmail.com>
2024-12-01 08:27:29 -0500

    2024, day 1

---
+++ 2024/rust/benches/days.rs
@@ -0,0 +1,30 @@
+use aoc2024::*;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+macro_rules! day {
+    ($name:ident, $day:tt) => {
+        pub fn $name(c: &mut Criterion) {
+            let mut group = c.benchmark_group(format!("Day {}", $day));
+            let input =
+                std::fs::read_to_string(format!("../inputs/{}", $day)).unwrap();
+
+            group.bench_function("part 1", |b| {
+                b.iter(|| $name::part1(black_box(&input)))
+            });
+            group.bench_function("part 2", |b| {
+                b.iter(|| $name::part2(black_box(&input)))
+            });
+            group.finish();
+        }
+    };
+}
+
+day!(day01, "01");
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().significance_level(0.1).sample_size(25);
+    targets = day01
+}
+criterion_main!(benches);
---
+++ 2024/rust/src/day01.rs
@@ -0,0 +1,88 @@
+use std::collections::HashMap;
+
+#[must_use]
+pub fn part1(input: &str) -> usize {
+    let pairs = input
+        .lines()
+        .map(|line| parse_ints(line))
+        .collect::<Vec<(usize, usize)>>();
+    let (mut lefts, mut rights): (Vec<usize>, Vec<usize>) =
+        pairs.into_iter().unzip();
+
+    lefts.sort_unstable();
+    rights.sort_unstable();
+
+    let sorted_pairs = lefts.into_iter().zip(rights.into_iter());
+
+    sorted_pairs.into_iter().map(|(a, b)| a.abs_diff(b)).sum()
+}
+
+#[must_use]
+pub fn part2(input: &str) -> usize {
+    let pairs = input
+        .lines()
+        .map(|line| parse_ints(line))
+        .collect::<Vec<(usize, usize)>>();
+    let (lefts, rights): (Vec<usize>, Vec<usize>) = pairs.into_iter().unzip();
+
+    let lefts_with_counts = unique_values_with_counts(lefts);
+    let rights_with_counts = unique_values_with_counts(rights);
+
+    lefts_with_counts
+        .into_iter()
+        .map(|(value, left_count)| match rights_with_counts.get(&value) {
+            Some(right_count) => value * left_count * right_count,
+            None => 0,
+        })
+        .sum()
+}
+
+fn unique_values_with_counts(values: Vec<usize>) -> HashMap<usize, usize> {
+    let mut map: HashMap<usize, usize> = HashMap::new();
+
+    for value in values.into_iter() {
+        *map.entry(value).or_insert(0) += 1;
+    }
+
+    map
+}
+
+fn parse_ints(line: &str) -> (usize, usize) {
+    let ints = line
+        .splitn(2, " ")
+        .map(|part| match part.trim().parse::<usize>() {
+            Ok(value) => value,
+            Err(_error) => panic!("error parsing input: {}", line),
+        })
+        .collect::<Vec<usize>>();
+
+    match ints.as_slice() {
+        [a, b] => (*a, *b),
+        _ => panic!("invalid input: {}", line),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn input() -> &'static str {
+        "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
+    }
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(input()), 11)
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(input()), 31)
+    }
+}
